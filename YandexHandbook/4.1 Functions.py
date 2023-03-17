52a88e5532da33527b4a0526a5e967085d8670a3
Sergey Nemenov <127150020+NemSer@users.noreply.github.com>
2023-03-17 16:02:35 +0300

    Update 4.1 Functions.py

--- YandexHandbook/4.1 Functions.py
+++ YandexHandbook/4.1 Functions.py
@@ -81,4 +81,99 @@
 # G
 
 # def can_eat(coordinates1: tuple, coordinates2: tuple):
+#     x = coordinates1[0]
+#     y = coordinates1[1]
+#     variants = [(x + 1, y + 2),
+#                 (x + 2, y + 1),
+#                 (x + 2, y - 1),
+#                 (x + 1, y - 2),
+#                 (x - 1, y - 2),
+#                 (x - 2, y - 1),
+#                 (x - 2, y + 1),
+#                 (x - 1, y + 2)]
+#     if coordinates2 in variants:
+#         return True
+#     else:
+#         return False
+
+# H
+
+# def is_palindrome(object):
+#     if isinstance(object, int):
+#         if len(str(object)) % 2 != 0:
+#             if str(object)[0:len(str(object)) // 2] == str(object)[len(str(object)) // 2 + 1:][::-1]:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             if str(object)[0:len(str(object)) // 2] == str(object)[len(str(object)) // 2:][::-1]:
+#                 return True
+#             else:
+#                 return False
+#     elif isinstance(object, str):
+#         if len(object) % 2 != 0:
+#             if object[0:len(object) // 2] == object[len(object) // 2 + 1:][::-1]:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             if object[0:len(object) // 2] == object[len(object) // 2:][::-1]:
+#                 return True
+#             else:
+#                 return False
+#     elif isinstance(object, tuple):
+#         if len(object) % 2 != 0:
+#             if object[0:len(object) // 2] == object[len(object) // 2 + 1:][::-1]:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             if object[0:len(object) // 2] == object[len(object) // 2:][::-1]:
+#                 return True
+#             else:
+#                 return False
+#     elif isinstance(object, list):
+#         if len(object) % 2 != 0:
+#             if object[0:len(object) // 2] == object[len(object) // 2 + 1:][::-1]:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             if object[0:len(object) // 2] == object[len(object) // 2:][::-1]:
+#                 return True
+#             else:
+#                 return False
+
+# I
+
+# def is_prime(number: int):
+#     if number != 1:
+#         for i in range(2, int(number ** (1 / 2)) + 1):
+#             if number % i == 0:
+#                 return False
+#         return True
+#     else:
+#         return False
+
+# J
+
+# def merge(cortej1: tuple, cortej2: tuple):
+#     i, j = 0, 0
+#     c = []
+#     while i != len(cortej1) and j != len(cortej2):
+#         if cortej1[i] > cortej2[j]:
+#             c.append(cortej2[j])
+#             j += 1
+#         elif cortej1[i] < cortej2[j]:
+#             c.append(cortej1[i])
+#             i += 1
+#         else:
+#             c.extend([cortej1[i], cortej2[j]])
+#             i += 1
+#             j += 1
+#     if i == len(cortej1):
+#         c.extend(cortej2[j::])
+#     elif j == len(cortej2):
+#         c.extend(cortej1[i::])
+#     return tuple(c)
 
